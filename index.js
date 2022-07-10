cb402dc691cab624dd949fb43693c36508dd09f7
Kyle Vermillion <kvermillion23@gmail.com>
2022-07-10 12:09:37 -0400

    commit tree 7ad991cddda1d7af23975dd72c6d08a1455dfbac

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+let idontknow = drivers.filter((findMatching) => findMatching.toUpperCase() === name.toUpperCase())
+    return idontknow
+}
+
+function fuzzyMatch(array, letters) {
+let newDrivers = array.filter(array => array.startsWith(letters))
+    return newDrivers
+
+
+}
+
+function matchName(array, string){
+   return array.filter(o => Object.keys(o).some(k => String(o[k]).toLowerCase().includes(string.toLowerCase())))
+}
